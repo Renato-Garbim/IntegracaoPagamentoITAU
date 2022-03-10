7bc76d57988e6f0a7e61bb19e4d601c7e93fb8d7
RenatoGarbim <renatobordinigarbim@hotmail.com>
2022-03-10 16:30:56 -0300

    limpeza controller

--- MockProdutosIntegracaoPagamento/Controllers/HomeController.cs
+++ MockProdutosIntegracaoPagamento/Controllers/HomeController.cs
@@ -81,11 +81,6 @@ namespace MockProdutosIntegracaoPagamento.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
